cf368a0e6ad2afc62dfb311f4e4b54856ca764a1
mistaj-ok <nickfury.jones@gmail.com>
2022-06-25 17:07:42 -0400

    added color, body structure, and image homepage only

---
+++ myscript.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+
+// const leftContainer = document.createElement('div');
+// const rightContainer = document.createElement('div');
+
+
+// leftContainer.classList.add('left-side');
+// rightContainer.classList.add('right-side');
+
+
+
+// //add an image to the left side
+const heroImg = document.createElement('img');
+heroImg.src = "./img/left-portrait.jpg";
+// leftContainer.appendChild(heroImg); 
+// rightContainer.textContent = "here is some filler text";
+// //create a header element
+// const header = document.querySelector('header');
+// // header content
+// const logo = document.createElement('div');
+// logo.classList.add('logo');
+// logo.textContent = "cornmuffins"
+// header.appendChild(logo);
+// // document.querySelector('header').appendChild(header);
