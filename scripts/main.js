ffc7275eed916a7fb7f1705f1df72e236ba84ecc
mistaj-ok <nickfury.jones@gmail.com>
2022-07-12 21:17:01 -0400

    removed extra script and css files, added mouseover to nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,10 @@
-const navDropdown = document.querySelectorAll('.r-links');
+// navigation activities
+const projects = document.querySelector('.r-links li');
+const dropdown = document.querySelector('.dropdown');
 
-function testPrint() {
-  console.log('hi welcome to chilis')
-}
-function toggleMenuExpand() {
-  this.classList.toggle('expanded')
-  console.log(this.classList);
-}
+dropdown.ul.setAttribute('opacity', '1');
 
-// loop through list items add event listener
-navDropdown.forEach(link => {link.addEventListener('mouseover', toggleMenuExpand)});
+// projects.addEventListener('click', 
 
-
-
-
-/* video player */
-
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const toggle = player.querySelector('.toggle');
-
-
-function togglePlay() {
-    console.log('hi welcome to chilis');
-
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
-  }
-
-
-  function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    console.log(icon);
-    toggle.textContent = icon;
-  }
-
-  video.addEventListener('click', togglePlay);
-  video.addEventListener('play', updateButton);
-  video.addEventListener('pause', updateButton);  
-
-  toggle.addEventListener('click', togglePlay);
-
-// add listener to each image box to register a click
-const imgBoxes = document.querySelectorAll('.img-box');
-function toggleOpen() {
-  this.classList.toggle('open');
-  console.log('open toggled');
-}
-imgBoxes.forEach(imgBox => imgBox.addEventListener('click', toggleOpen));
-
-
-// make the clicked box larger for preview
 
 
